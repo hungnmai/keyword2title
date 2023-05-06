0cf4feef31e56d4afe420a7bfd51af3e7c8d95e1
Hung Nguyen <hungmanh.hust@gmail.com>
2023-05-06 23:09:49 +0700

    increase worker

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import uvicorn
 if __name__ == "__main__":
-    config = uvicorn.Config("routers_1:app", host='0.0.0.0', port=5000, log_level="info")
+    config = uvicorn.Config("routers_1:app", host='0.0.0.0', port=5000, log_level="info", workers=4)
     server = uvicorn.Server(config)
     server.run()
